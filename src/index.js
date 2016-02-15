a4e26e0dbd2f85a83fa29ffd776ea74b837ec4b0
andrey <andrey.ivanov@saltpepper.ru>
2016-02-15 16:17:43 +0300

    Add pre

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+// import App from 'app';
+
+console.log('shit!')
+require('./src/app');
+
+
+
+console.log(App);
+// React.render(<App />, document.getElementById('root'));
