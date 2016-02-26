22ddc1ceeb654513ebf49861e6cecf1ede36d062
andrey <andrey.ivanov@saltpepper.ru>
2016-02-26 15:35:58 +0300

    Add build

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,14 +4,27 @@ import ReactDOM from 'react-dom';
 import {Header} from './layouts/header/header';
 import {Content} from './layouts/content/content';
 import {Footer} from './layouts/footer/footer';
+// import 'normalize.css';
 // import 'reset.css';
 import indexCss from './../stylesheets/index.css';
 
-let wait = (time) => {
-  return new Promise((resolve, reject)=> setTimeout(resolve, time));
+const wait = (time) => {
+  return new Promise(
+    (resolve, reject)=> {
+      setTimeout(resolve, time);
+    }
+  );
 }
 
-wait(3000).then(()=> console.log('wait yo!'));
+// wait(3000).then(()=> console.log('wait yo!'));
+
+wait(1000).then(()=> {
+  console.log('step 1!');
+  return wait(3000);
+}).then(()=> {
+  console.log('step 2!');
+  return wait(2000);
+}).then(()=> console.log('step 3!'));
 
 // import {Header, Content, Footer} from './layouts';
 
--- client/scripts/layouts/content/content.js
+++ client/scripts/layouts/content/content.js
@@ -12,23 +12,61 @@ const Example = React.createClass({
   }
 })
 
-const lorem = `Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-               Ut enim ad minim veniam, quis nostrud exercitation
-               ullamco laboris nisi ut aliquip ex ea commodo
-               consequat. Duis aute irure dolor in
-               reprehenderit in voluptate velit esse cillum
-               dolore eu fugiat nulla pariatur.
-               Excepteur sint occaecat cupidatat non proident,
-               sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+class Input extends React.Component {
+  getInitialState () {
+    return {value: '', placeholder: this.props.placeholder};
+  }
+  handleChange (event) {
+    console.log(event);
+    this.setState.value = event.target.value;
+  }
+  render () {
+    return (
+      <div>
+        <input type="text" placeholder={this.props.placeholder} onChange={this.handleChange}/>
+      </div>
+    )
+  }
+}
+
+class List extends React.Component {
+  handleAdd (event) {
+    // console.log(this.state);
+    // this.props.results.push({
+    //   id: _.uniqueId(),
+    //   text: event.target.value
+    // })
+  }
+  render () {
+    var results = this.props.results;
+    return (
+      <div>
+        <Input type="text" />
+        <button onClick={this.handleAdd}>Добавить</button>
+        <ol>
+          {results.map(function(result) {
+            return <li key={result.id}>{result.text}</li>;
+          })}
+        </ol>
+      </div>
+    )
+  }
+}
+
+var results = [
+  {id: 1, text: 'Hello 1'},
+  {id: 2, text: 'Hello 2'},
+  {id: 3, text: 'Hello 3'},
+  {id: 4, text: 'Hello 4'},
+  {id: 5, text: 'Hello 5'}
+]
 
 export class Content extends React.Component {
   render () {
     return (
       <main className={style.Content}>
-        <p>{lorem}</p>
-        <p>{lorem}</p>
-        <p>{lorem}</p>
+        <Input placeholder="hello"/>
+        <List results={results} />
         <Example/>
       </main>
     )
