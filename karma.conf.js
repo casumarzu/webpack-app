285b30a1a5c9a9b04a7bff08196a213230e282ce
andrey <andrey.ivanov@saltpepper.ru>
2016-03-09 15:07:22 +0300

    Add karma test

---
+++ karma.conf.js
@@ -0,0 +1,24 @@
+module.exports = function(config) {
+  config.set({
+    files: [
+      './client/**/*.test.js',
+    ],
+
+    preprocessors: {
+      './client/**/*.test.js': ['webpack']
+    },
+
+    webpack: {
+    },
+
+    webpackMiddleware: {
+      noInfo: true
+    },
+    plugins: [
+      'karma-jasmine',
+      'karma-webpack',
+      'karma-sourcemap-loader',
+      'karma-chrome-launcher'
+    ]
+  });
+};
