fb0ab6ab2b24d813ba102f2dab3bcdb2467f369a
andrey <casumarzu@yandex.ru>
2016-03-21 14:43:45 +0300

    Add flux pre

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './scripts/app';
-ReactDOM.render(<App />, document.getElementById('root'));
+import AppContainer from './scripts/components/AppContainer';
+ReactDOM.render(<AppContainer />, document.getElementById('root'));
